7649cf2a61cba6a8be3a7e94d81c7fb6fa449ac4
Tristan Sloughter <t@crashfast.com>
2013-09-15 14:29:54 -0500

    add deps and compile to makefile and move start deps to _app module

--- src/erldocker_app.erl
+++ src/erldocker_app.erl
@@ -2,16 +2,32 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0,
+         start/2,
+         stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+start() ->
+    start_deps(erldocker, permanent).
+
+start_deps(App, Type) ->
+    case application:start(App, Type) of
+        ok ->
+            ok;
+        {error, {not_started, Dep}} ->
+            start_deps(Dep, Type),
+            start_deps(App, Type)
+    end.
+
 start(_StartType, _StartArgs) ->
     {ok, _Pid} = hackney:start_pool(erldocker_pool, [{timeout, 150000}, {pool_size, 1}]),
 
     erldocker_sup:start_link().
+
+
 
 stop(_State) ->
     hackney:stop_pool(erldocker_pool),
